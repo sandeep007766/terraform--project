e980b452a63eec36dc132a593636a78f1a19cf18
root <root@sam.oa5jrutdt23e1psrfyqtuim05g.oslx.internal.cloudapp.net>
2025-09-19 16:25:57 +0000

    updated config for linux vm

---
+++ 07_Linux_VM/ssh-keys/c1-versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "1.10.4"
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">4.0.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">3.0"
+    }
+  }
+}
+
+provider "azurerm"  {
+  features {}
+  subscription_id = "00681e52-1746-42b2-b0a5-ad83a3b482af"
+}
