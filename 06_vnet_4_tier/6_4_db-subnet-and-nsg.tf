800230b6281ae67b1e94d44574508b05ced56ddb
Sandeep Dharmireddy <109214088+sandeep007766@users.noreply.github.com>
2025-09-19 22:41:19 +0530

    adding tf config for basics linux-vm and vnet

--- 06_vnet_4_tier/6_4_db-subnet-and-nsg.tf
+++ 06_vnet_4_tier/6_4_db-subnet-and-nsg.tf
@@ -1,4 +1,4 @@
-# Resource-1: Create DBTier Subnet
+# Resource-1: Create DBTier Subnet 
 resource "azurerm_subnet" "dbsubnet" {
   name                 = "${azurerm_virtual_network.vnet.name}-${var.db_subnet_name}"
   resource_group_name  = azurerm_resource_group.rg.name
