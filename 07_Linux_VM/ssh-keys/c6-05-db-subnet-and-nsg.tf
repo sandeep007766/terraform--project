e0a9d987bfdde4423078f03bdc99ec19b753be2d
Sandeep Dharmireddy <109214088+sandeep007766@users.noreply.github.com>
2025-09-19 22:23:45 +0530

    adding tf config for basics linux-vm and vnet

--- 07_Linux_VM/ssh-keys/c6-05-db-subnet-and-nsg.tf
+++ 07_Linux_VM/ssh-keys/c6-05-db-subnet-and-nsg.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "dbsubnet" {
 # Resource-2: Create Network Security Group (NSG)
 resource "azurerm_network_security_group" "db_subnet_nsg" {
   name                = "${azurerm_subnet.dbsubnet.name}-nsg"
-  location            = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location 
   resource_group_name = azurerm_resource_group.rg.name
 }
 
