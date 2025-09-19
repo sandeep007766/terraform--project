7323f109fdbdce7c9a3608714dc789044fcaff60
Sandeep Dharmireddy <109214088+sandeep007766@users.noreply.github.com>
2025-09-19 22:40:24 +0530

    adding tf config for basics linux-vm and vnet

--- 06_vnet_4_tier/6_2_-web-subnet-and-nsg.tf
+++ 06_vnet_4_tier/6_2_-web-subnet-and-nsg.tf
@@ -1,4 +1,4 @@
-# Resource-1: Create WebTier Subnet
+# Resource-1: Create WebTier Subnet 
 resource "azurerm_subnet" "websubnet" {
   name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
   resource_group_name  = azurerm_resource_group.rg.name
